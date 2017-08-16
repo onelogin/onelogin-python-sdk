415982f0467c634b02a610b32af26bce6ff95b95
Sixto Martin <pitbulk@gmail.com>
2017-08-16 21:14:52 +0200

    extension_required attribute of App now is extension

--- src/onelogin/api/models/app.py
+++ src/onelogin/api/models/app.py
@@ -9,7 +9,7 @@ class App(object):
         self.icon = data.get('icon', '')
         provisioned = data.get('provisioned', '')
         self.provisioned = int(provisioned) if provisioned is not None else None
-        extension_required = data.get('extension_required', None)
+        extension_required = data.get('extension', None)
         self.extension_required = extension_required.lower() in ("yes", "true", "1") if extension_required is not None else None
         login_id = data.get('login_id', None)
         self.login_id = int(login_id) if login_id is not None else None
