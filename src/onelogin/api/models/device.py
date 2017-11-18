b5aaaaacad4b27a499d3dce744c4fc6227a4c62d
Przemysław Dąbek <przemyslaw.dabek@uxpin.com>
2017-11-18 18:05:01 +0100

    added extra two fields for Duo Security device type

--- src/onelogin/api/models/device.py
+++ src/onelogin/api/models/device.py
@@ -5,3 +5,5 @@ class Device(object):
     def __init__(self, data):
         self.id = data.get('device_id', None)
         self.type = data.get('device_type', '')
+        self.duo_api_hostname = data.get('duo_api_hostname', None)
+        self.duo_sig_request = data.get('duo_sig_request', None)
