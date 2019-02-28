3d241eb30545d0085aa839bf7a9c69ade12065e5
Sixto Martin <pitbulk@gmail.com>
2019-02-28 11:25:30 +0100

    Replace at event_type object the reference by the description field

--- src/onelogin/api/models/event_type.py
+++ src/onelogin/api/models/event_type.py
@@ -5,4 +5,4 @@ class EventType(object):
     def __init__(self, data):
         self.id = data.get('id', None)
         self.name = data.get('name', '')
-        self.reference = data.get('reference', '')
+        self.description = data.get('description', '')
